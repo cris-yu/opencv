1bdff5b3f924d05e84213188a89bd49dc34c3bd4
cris-yu <cris_yu67@163.com>
2021-05-06 16:19:23 +0800

    更新备注

--- chapter1.py
+++ chapter1.py
@@ -1,21 +1,26 @@
 import cv2
 #print("package imported!")
-#show picture
-#img = cv2.imread("resouces/未命名作品.png")
 
+#展示图片
+'''
+img = cv2.imread("resouces/未命名作品.png")
 #cv2.imshow("output",img)
 #cv2.waitKey(0)
-
+'''
 
 
 #放视频
-#cap=cv2.VideoCapture("resouces/fingers.mov")
-#while True:
-   # success,img=cap.read()
-    #cv2.imshow("Video",img)
-   # if cv2.waitKey(1)&0xFF==ord('q'):
-     #   break
+'''
+cap=cv2.VideoCapture("resouces/fingers.mov")
+while True:
+    success,img=cap.read()
+    cv2.imshow("Video",img)
+    if cv2.waitKey(1)&0xFF==ord('q'):
+        break
+'''
 
+#调用摄像头
+'''
 cap=cv2.VideoCapture(0)
 cap.set(3,640)
 
@@ -24,3 +29,4 @@ while True:
     cv2.imshow("cap", img)
     if cv2.waitKey(1)&0xFF==ord('q'):
         break
+'''
